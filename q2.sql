db721946256c024badc18eb64e01422b6123e679
daniel-chang-github <81652137+daniel-chang-github@users.noreply.github.com>
2022-03-21 23:13:08 -0400

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -18,4 +18,4 @@ SELECT name FROM Student WHERE id BETWEEN @v2 AND @v3;
 --     -> Table scan on Student  (cost=41.00 rows=400) (actual time=0.022..0.238 rows=400 loops=1)
 -- '
 
--- Optimization not necessary? Index is not being used. Is it becuase the query is searching for a range?
+-- Crated an index on the Student table. However, the index is not being used. Is it becuase the query is searching for a range?
